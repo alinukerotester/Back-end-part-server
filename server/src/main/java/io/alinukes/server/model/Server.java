c9a57e9f544542290a167ccb40ab2f4ba5df366b
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2022-11-14 17:28:03 +0200

    More files

--- server/src/main/java/io/alinukes/server/model/Server.java
+++ server/src/main/java/io/alinukes/server/model/Server.java
@@ -1,5 +1,13 @@
 package io.alinukes.server.model;
 
+import io.alinukes.server.enumeration.Status;
+
 public class Server {
     private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
 }
